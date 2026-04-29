cb3accf7126024e45aa0ff5d0edbeb486358192e
Pierre Poulain <pierre.poulain@cupnet.net>
2026-04-29 16:58:12 +0200

    feat: Add response time for endpoint

--- webapp/app/main.py
+++ webapp/app/main.py
@@ -1,6 +1,7 @@
 import pathlib
+import time
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 
 from .frontend.controller import router as frontend_router
@@ -15,6 +16,21 @@ print(f"Running FastAPI app from: {pathlib.Path().absolute()}")
 # Create FastAPI app
 app = FastAPI(title="MDverse")
 app.mount("/static", StaticFiles(directory="webapp/static"), name="static")
+
+
+# Middleware to measure endpoint response time.
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    end_time = time.perf_counter()
+    execution_time_ms = (end_time - start_time) * 1000
+    # Add the execution time to the response headers for clients to see
+    response.headers["X-Process-Time-Ms"] = str(execution_time_ms)
+    # Print the execution time.
+    print(f"Request to '{request.url.path}' took {execution_time_ms:.4f} ms")
+    return response
+
 
 # Frontend endpoints
 app.include_router(frontend_router)
